59c361095b7b19447c8bab81ba9653b239e7fa39
Giuliana Fertonani <giuly@Giulianas-MacBook-Pro.local>
2022-04-18 15:02:03 -0300

    Project with Axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,8 @@ import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import Form from './components/Form';
 import List from './components/List';
-import {Sub} from '../types';
-
-const INITIAL_STATE = [
-  {
-    nick: 'dapelu',
-    subMonths: 3,
-    avatar: 'https://i.pravatar.cc/150?u=dapelu',
-    description: 'Dapelu hace de moderador a veces'
-  },
-  {
-    nick:'Sergio_serrano',
-    subMonths:7,
-    avatar:'https://i.pravatar.cc/150?u=sergio_serrano'
-  }
-]
-
-
+import {Sub, SubsResponseFromApi} from '../types';
+import axios from 'axios';
 
 interface AppState {
   subs: Array<Sub>
@@ -31,17 +16,46 @@ function App() {
   const divRef = useRef<HTMLDivElement>(null);
   
   useEffect(()=>{
-    setSubs(INITIAL_STATE)
+    const fetchSubs = () => {
+      return axios
+      .get<SubsResponseFromApi>('http://localhost:3001/subs')
+      .then(response => response.data)
+    
+    }
+
+    const mapFromApiToSubs = (apiResponse: SubsResponseFromApi):
+    Array<Sub> => {
+      return apiResponse.map(subFromApi => {
+        const {
+          months: subMonths,
+          profileUrl: avatar,
+          nick,
+          description
+        } = subFromApi
+        return{
+          nick,
+          description,
+          avatar,
+          subMonths
+      }
+    })
+  }
+    
+    fetchSubs()
+    .then(mapFromApiToSubs)
+    .then(setSubs)
   }, [])
 
   const handlerNewSub = (newSub: Sub): void => {
     setSubs(subs => [...subs, newSub])
+    setNewSubsNumber(n => n + 1)
   }
   
   return (
     <div className="App" ref={divRef}>
       <h1>midu subs</h1>
       <List subs={subs}/>
+      New Subs: {newSubsNumber}
       <Form onNewSub={handlerNewSub}/>
     </div>
   );
